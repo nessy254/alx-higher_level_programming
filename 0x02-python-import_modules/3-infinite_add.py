2857aaa20129cbae0f210b7f5dc2e95b6c7fdd68
nessy254 <mukaminessy9@gmail.com>
2023-12-05 12:05:49 +0300

    mandatory tasks

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    n = len(argv)
+    result = 0
+    for i in range(1, n):
+        result += int(argv[i])
+    print("{}".format(result))
