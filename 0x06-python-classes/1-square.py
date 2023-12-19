9845f0c28335096df9755ffe04f822c490248e40
nessy254 <mukaminessy9@gmail.com>
2023-12-20 00:09:07 +0300

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+Another Square module
+
+With a private attribute
+"""
+
 
 class Square:
+    """
+    This is Square class
+    """
     def __init__(self, size):
+        """
+        The __init__method
+        """
         self.__size = size
