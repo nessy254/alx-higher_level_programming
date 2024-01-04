6e3c5b28b163a3defcf7f406b398ab59d5d33e31
nessy254 <mukaminessy9@gmail.com>
2024-01-04 23:12:49 +0300

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
    Private instance attribute: width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,10 +17,12 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """set width"""
-        if value >= 0:
-            self.__width = value
-        else:
-            raise Exception("width must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = value
+
     @property
     def height(self):
         """get height"""
@@ -29,8 +31,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set height"""
-        if value >= 0:
-            self.__height = value
-        else:
-            raise Exception("height must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("weight must be >= 0")
+        self.__height = value
 
