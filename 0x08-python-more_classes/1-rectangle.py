dab115e49ff35f6bda37708b4c93e643929aa5af
nessy254 <mukaminessy9@gmail.com>
2024-01-05 20:32:25 +0300

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 """
 defines a rectangle by a private instance attribute width and height
 """
 
 class Rectangle:
     """The defined class Rectangle"""
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         """Initialized rectangle"""
         self.width = width
         self.height = height
@@ -27,7 +26,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """get height"""
+        """geit height"""
         return self.__height
 
     @height.setter
