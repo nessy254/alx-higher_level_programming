ad2e617e74c5f9683a872a257f96fe612ccb4df3
nessy254 <mukaminessy9@gmail.com>
2024-01-06 10:06:18 +0300

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(J=[]):
-      J += ["BestSchool"]
-      return (", ".join(J))
+def magic_string():
+    setattr(magic_string, "n", getattr(magic_string, "n", 0) + 1)
+    return ("BestSchool, " * getattr(magic_string, "n", 0))[:-2]
