c9a39cd85554983f9d7a6bde902bf423b21a6361
nessy254 <mukaminessy9@gmail.com>
2024-01-08 19:35:27 +0300

    inheritance tasks

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Defines a class MyInt that inherits from int."""
+
+
+
+
+
+class MyInt(int):
+
+    """Inverts the int operators == and !=."""
+
+
+
+    def __eq__(self, value):
+
+        """Overrides == operator with != behavior."""
+
+        return self.real != value
+
+
+
+    def __ne__(self, value):
+
+        """Overrides != operator with == behavior."""
+
+        return self.real == value
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    6-base_geometry: class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        baseGeometry
+        Attributes: None.
+        Methods:
+            area() - raises an exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
