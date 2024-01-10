44de5c8f92b99f5e9a1fb9ec8bd9846116967776
nessy254 <mukaminessy9@gmail.com>
2024-01-10 22:10:40 +0300

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-    """
-        Module that contains a function that writes to a text file
-    """
+"""
+function that writes a string to a text file
+"""
 
 
 def write_file(filename="", text=""):
-	"""
-    Function that writes to a text file
-    Args:
-        filename: filename
-        text: text to write
-
-    Raises
-        Exception: when the file can be opened
-	"""
-
-	with open(filename, 'w', encoding="utf-8") as f:
-		return f.write(text)
+    """writes a string to a text file"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text)
