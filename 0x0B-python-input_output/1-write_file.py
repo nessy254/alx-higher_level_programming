d1ac34fe0784730783019adc5ae7807fc3abe227
nessy254 <mukaminessy9@gmail.com>
2024-01-10 22:00:10 +0300

    input/output tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" Module that contains a function that writes to a text file
-
-"""
+    """
+        Module that contains a function that writes to a text file
+    """
 
 
 def write_file(filename="", text=""):
-	""" Function that writes to a text file
-
+	"""
+    Function that writes to a text file
     Args:
-
         filename: filename
-
         text: text to write
 
     Raises
-
         Exception: when the file can be opened
 	"""
 
